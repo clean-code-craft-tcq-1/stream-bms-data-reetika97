ce2eeb24fffa9c8eb6abeb8bfdd3ab00fff10699
reetika97 <77428788+reetika97@users.noreply.github.com>
2021-06-01 10:31:17 +0530

    Create stream_test.py

---
+++ stream_test.py
@@ -0,0 +1,11 @@
+import unittest
+import generate_stream
+
+class GenerateStreamTest(unittest.TestCase):
+  def test_within_range_temp(self):
+    TestReading=generate_stream.generate_param_reading('temperature')
+    self.assertTrue(TestReading>=generate_stream.Battery_parameters_range['temperature'][0] && TestReading<=generate_stream.Battery_parameters_range['temperature'][1])
+  
+   
+if __name__ == '__main__':
+  unittest.main()
